52bba4ad25a71c5c633fd264b5efb34bc42d3e5d
mubashir-123 <mubashiransa8@gmail.com>
2023-10-29 12:06:33 +0500

    JS Math Expression

---
+++ javascript/class02/app.js
@@ -0,0 +1,9 @@
+//TASK 1
+let num1,num2;
+num1 = +prompt('Enter num 1');
+num2 = +prompt('Enter num 2');
+alert("The sum of "+num1+" and "+num2+" is "+(num1+num2));
+alert("The subtraction of "+num1+" and "+num2+" is "+(num1-num2));
+alert("The multiplication of "+num1+" and "+num2+" is "+(num1*num2));
+alert("The division of "+num1+" and "+num2+" is "+(num1/num2));
+alert("The modulus of "+num1+" and "+num2+" is "+(num1%num2));
